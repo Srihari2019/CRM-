595a743a6065b91e966250c5ce580e5925957620
Srihari <psrihari2019@gmail.com>
2023-12-13 15:34:38 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>CRM Project using PHP and MySQL</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/one-page-wonder.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top" style="background-color:#512DA8">
+    <div class="container">
+      <a class="navbar-brand" href="#">CRM</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="registration.php">User Sign Up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">User Log In</a>
+          </li>
+
+            <li class="nav-item">
+            <a class="nav-link" href="admin/">Admin</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <header class="jumbotron text-white mt-5 mb-0" style="background-color:#9575CD">
+    <div class="container">
+      <div class="row">
+        <div class="col-6 col-sm-4">
+          <h4>Customer Relationship Management</h4>
+          The customer management system is about the customer requesting for query tickets and creating quotes for the required services. The admin manages the tickets and quotes that are requested by the customer . The admin manages the customer details and login details of the customer.
+
+        </div>
+        <div class="offset-2 col-4 col-sm-3 mt-5">
+        <a href="#"><img src="assets/img/logo.jpg" alt="logo" height="130" width="120"/></a>
+        </div>
+        <div class="col-12 col-sm-3 mt-4">
+        <a href="registration.php" class="btn btn-primary btn-xl rounded-pill mt-5 bg-lights">User Signup</a>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!--
+  <div class="masthead text-center text-white">
+    <div class="masthead-content">
+      <div class="container">
+        <h1 class="masthead-heading mb-0">Customer Relationship Management</h1>
+        
+        <a href="registration.php" class="btn btn-primary btn-xl rounded-pill mt-5">User Signup</a>
+      </div>
+    </div>
+    <div class="bg-circle-1 bg-circle"></div>
+    <div class="bg-circle-2 bg-circle"></div>
+    <div class="bg-circle-3 bg-circle"></div>
+    <div class="bg-circle-4 bg-circle"></div>
+</div> -->
+
+
+  <!-- Footer -->
+  <footer class="py-5" style="background-color:#D1C4E9">
+    <div class="container">
+      <p class="m-1 text-center text-white small">Copyright &copy; SSV Team 2023</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
